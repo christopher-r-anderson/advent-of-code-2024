621b7288c9a7433f959606a25c6acb1d450972ed
Christopher Anderson <src@christopheranderson.net>
2024-12-02 09:14:42 -0500

    Day 02, Part 1

--- day-02/src/part1.rs
+++ day-02/src/part1.rs
@@ -1,6 +1,27 @@
+use itertools::Itertools;
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<usize> {
+    let count = input
+        .lines()
+        .map(|report| report
+            .split_whitespace()
+            .map(|level| level.parse::<i32>().expect("Input should be integers"))
+            .collect::<Vec<_>>()
+        )
+        .filter(|levels| {
+            let diffs = levels
+                .iter()
+                .tuple_windows()
+                .map(|(a, b)| b - a)
+                .collect::<Vec<_>>();
+            diffs.iter().all(|diff| diff.abs() > 0 && diff.abs() < 4)
+            &&
+            diffs.iter().map(|diff| diff.cmp(&0)).all_equal()
+        })
+        .collect::<Vec<_>>()
+        .len();
+    Ok(count)
 }
 
 #[cfg(test)]
@@ -9,9 +30,13 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+        assert_eq!(2, process(input)?);
         Ok(())
     }
 }
