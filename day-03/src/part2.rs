2e416096db574fd84e6f4a68d6ad99093fff77ed
Christopher Anderson <src@christopheranderson.net>
2024-12-04 00:57:22 -0500

    Day 03, Part 2

--- day-03/src/part2.rs
+++ day-03/src/part2.rs
@@ -1,6 +1,39 @@
+use miette::IntoDiagnostic;
+use regex::Regex;
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 2");
+pub fn process(input: &str) -> miette::Result<u32> {
+    let re = Regex::new(r"do\(\)|don't\(\)|mul\((\d+),(\d+)\)").into_diagnostic()?;
+    let mut total = 0u32;
+    let mut enabled = true;
+    for captures in re.captures_iter(input) {
+        if let Some(m) = captures.get(0) {
+            match m.as_str() {
+                "do()" => {
+                    enabled = true;
+                }
+                "don't()" => {
+                    enabled = false;
+                }
+                _ => {
+                    if enabled {
+                        let a = captures.get(1)
+                            .expect("mul() regex has two submatches")
+                            .as_str()
+                            .parse::<u32>()
+                            .into_diagnostic()?;
+                        let b = captures.get(2)
+                            .expect("mul() regex has two submatches")
+                            .as_str()
+                            .parse::<u32>()
+                            .into_diagnostic()?;
+                        total += a * b;
+                    }
+                }
+            }
+        }
+    }
+    Ok(total)
 }
 
 #[cfg(test)]
@@ -9,9 +42,8 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        assert_eq!(48, process(input)?);
         Ok(())
     }
 }
