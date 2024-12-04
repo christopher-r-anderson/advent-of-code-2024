eef4d677e11fedf3f78d0bbbddec864bcb90785b
Christopher Anderson <src@christopheranderson.net>
2024-12-04 00:11:31 -0500

    Day 03, Part 1

--- day-03/src/part1.rs
+++ day-03/src/part1.rs
@@ -1,6 +1,14 @@
+use miette::IntoDiagnostic;
+use regex::Regex;
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<String> {
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").into_diagnostic()?;
+    let mut total = 0u32;
+    for (_, [a, b]) in re.captures_iter(input).map(|c| c.extract()) {
+        total += a.parse::<u32>().unwrap() * b.parse::<u32>().unwrap();
+    }
+    Ok(total.to_string())
 }
 
 #[cfg(test)]
@@ -9,9 +17,8 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        assert_eq!("161", process(input)?);
         Ok(())
     }
 }
